dc282e6967bc69f1e27d99aac6a4511a13bd9eb0
J-Morocho <jmorocho95@yahoo.com>
2020-07-14 16:40:43 -0400

    change menu visibility on hamburger click

--- webpage/app.js
+++ webpage/app.js
@@ -15,4 +15,27 @@ fetch(url)
                 url: entry.gsx$url.$t
              }
         })
+
+        console.log(projects)
     })
+
+
+
+function createProjectCard(project) {
+    const $card = $('<div>').addClass('proj-card')
+    const $proDivider = $('<div>').addClass('project-divider')
+    const $projHeader = $('<div>').addClass('project-header')
+    const $h3 = $('<h3>')
+    const $projectName = $h3.addClass('project-name').append($('<p>').text(project.name))
+    const $projectLinks = $h3.addClass('project-links')
+    $('#projects-container-flex').append($card)
+    console.log('card made')
+}
+
+
+function displayMenu() {
+    $('.menu').attr('id', 'active')
+    
+}
+
+$('.hamburger').on('click', displayMenu)
