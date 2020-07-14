50582cef3b4c4b8692be52fdf79b241c79f07e9e
J-Morocho <jmorocho95@yahoo.com>
2020-07-14 14:06:49 -0400

    add spreadsheet url

--- webpage/app.js
+++ webpage/app.js
@@ -5,8 +5,13 @@ $navIcon.on('click', () => {
         $('.menu-list').show()
     })
 
-    $navIcon.css('background-color', 'red')
+    $navIcon.css('background-color', '#717BD9')
 })
 
 // when contact-form flex width is greater than 960px hide
-
+// my projects sheet
+//https://docs.google.com/spreadsheets/d/1T9156QzG1d079lkMnl279c1ygoPHE6fsD6Apx0j7d6g/edit#gid=0
+//url to fetch from https://spreadsheets.google.com/feeds/list/1T9156QzG1d079lkMnl279c1ygoPHE6fsD6Apx0j7d6g/od6/public/values?alt=json
+const url = "https://spreadsheets.google.com/feeds/list/1T9156QzG1d079lkMnl279c1ygoPHE6fsD6Apx0j7d6g/od6/public/values?alt=json"
+fetch(url)
+    .then(response => response.json())
